03852499e7c2a72d2019a4f302e65aed29080d66
Yuto Watanabe <yuto.w51942@gmail.com>
2020-08-20 18:39:15 +0900

    added plot graaph

--- src/create.py
+++ src/create.py
@@ -1,5 +1,64 @@
-import matplotlib
+import datetime
+import os
+
+import matplotlib.pyplot as plt
+
+try:
+    from database import read_today
+except ImportError:
+    from .database import read_today
 
 
 def graph(data_file_path: str) -> str:
-    pass
+    '''
+    graph plot and save to image file.
+
+    Args:
+        data_file_path (str): database file path.
+
+    Returns:
+        str: generated image file path.
+    '''
+    today_data = read_today(data_file_path)
+
+    temps = []
+    hu = []
+
+    jst = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+    now = datetime.datetime.now(jst)
+
+    directory = 'graph_image'
+    # if directory not found it create directory.
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    image_file_path = os.path.join(directory, f'{now.strftime(r"%Y%m%d")}.png')
+
+    # Sort by date.
+    sorted_data = sorted(today_data, key=lambda x: x['date'])
+
+    for element in sorted_data:
+        temps.append(element['temp'])
+        hu.append(element['hu'])
+
+    # graph plot
+    fig = plt.figure(figsize=[6.4, 4.8])
+    ax1 = fig.add_subplot(111)
+    ax2 = ax1.twinx()
+
+    # line color
+    ax1.plot(temps, color='blue')
+    ax2.plot(hu, color='orange')
+
+    # lavel of y
+    ax1.set_ylabel('Temperature ($^\circ$C)')
+    ax2.set_ylabel('Humidity (%)')
+
+    ax1.grid(True)
+    ax1.set_xlabel('date')
+    ax1.tick_params(labelbottom=False, bottom=False)
+    ax2.tick_params(labelbottom=False, bottom=False)
+    plt.title(f'Graph of changes in room temperature and humidity on {now.strftime(r"%m/%d")}')
+
+    plt.savefig(image_file_path)
+
+    return image_file_path
--- src/main.py
+++ src/main.py
@@ -3,25 +3,30 @@ main
 
 Copyright (C) 2020 Yuto Watanabe
 '''
+import os
 from datetime import date
-
 
 try:
     # from operation import surveillance
-    from database import write, read_all, read_today
+    from database import write, read_all, create_table
+    from create import graph
 except ImportError:
     # from .operation import surveillance
-    from .database import write, read_all, read_today
+    from .database import write, read_all, create_table
+    from .create import graph
 
 
 def main():
     '''
     main function.
     '''
-    database = 'data.db'
-    write(database, 23, 60, 150, True)
 
-    print(read_today(database))
+    database = 'data.db'
+    if not os.path.isfile(database):
+        create_table(database)
+    write(database, 23, 40, 150, True)
+
+    graph(database)
 
 
 if __name__ == "__main__":
